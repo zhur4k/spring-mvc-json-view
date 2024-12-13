2c397399142a26355381b4160da9d2a11f836e1d
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-13 20:43:45 +0300

    feat:initial project.

---
+++ src/main/java/com/mvc/jsonview/SpringMvcJsonViewApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.jsonview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJsonViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcJsonViewApplication.class, args);
+    }
+
+}

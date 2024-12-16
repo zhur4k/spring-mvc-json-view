8d0cc6829d3d98e992415f340ed8b6ef035ce2d2
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-16 09:44:28 +0300

    feat:create user controller.

---
+++ src/main/java/com/mvc/jsonview/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.mvc.jsonview.controller;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.mvc.jsonview.model.User;
+import com.mvc.jsonview.model.Views;
+import com.mvc.jsonview.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @JsonView(Views.UserDetails.class)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    @JsonView(Views.UserDetails.class)
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/create")
+    public void createUser(@RequestBody User user) {
+        userService.createUser(user);
+    }
+
+    @PutMapping("/update")
+    public void updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}

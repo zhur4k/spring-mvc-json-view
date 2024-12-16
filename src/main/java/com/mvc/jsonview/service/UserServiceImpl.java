474a2197ab06daa1c4dbfff0e8f5616554a4a091
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-16 09:35:42 +0300

    feat:implement userService.

---
+++ src/main/java/com/mvc/jsonview/service/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mvc.jsonview.service;
+
+import com.mvc.jsonview.model.User;
+import com.mvc.jsonview.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+}
